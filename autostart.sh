4e3ce40283e65e62a6780f7d2e4969917469629e
Jose Ignacio Carretero <joseignacio.carretero@gmail.com>
2023-08-18 12:49:21 +0200

    all the commit together

--- autostart.sh
+++ autostart.sh
@@ -7,19 +7,24 @@ function run {
   fi
 }
 
+cd ~ 
+
+.local/bin/weather.sh
 run "nm-applet"
 # run "pamac-tray"
 run "megasync"
 # run "xfce4-power-manager"
 # run "blueberry-tray"
 # run "/usr/lib/xfce4/notifyd/xfce4-notifyd"
-run "volumeicon"
+# run "volumeicon"
+run "/usr/bin/tuxedo-control-center --tray"
 run "/usr/bin/dunst"
 run "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
+# run "xscreensaver"
 picom -b  --config ~/.dwm/picom.conf
 run sxhkd -c ~/.dwm/sxhkd/sxhkdrc
 ~/.screenlayout/layout.sh
 # Launch KRT - Pomodoro
-/home/jicg/Personal/KrtPomodoro/server_start.sh &
+# /home/jicg/Personal/KrtPomodoro/server_start.sh &
 nitrogen --restore
-run kbar &
+run krtbar &
